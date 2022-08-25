f67a7bdd808aea04af3b24fbd19172729281fec1
Joaomla <joao_mla@outlook.pt>
2022-08-25 21:26:28 +0100

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = 
+[
+    { id: 12, name: 'Psyclown' },
+    { id: 13, name: 'Flowerina' },
+    { id: 14, name: 'Imitattooist' },
+    { id: 15, name: 'Ice-Fang' },
+    { id: 16, name: 'Polterghost' },
+    { id: 17, name: 'Nocturna' },
+    { id: 18, name: 'Dr. Peanut' },
+    { id: 19, name: 'Bob' },
+    { id: 20, name: 'Lice-cream' }
+]
